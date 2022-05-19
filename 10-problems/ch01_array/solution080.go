f6d5a9c2ef85fca492cf088d5eab4466cdc92fc6
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-05-19 23:42:32 +0800

    10-problems/ch01_array/solution080.go

--- 10-problems/ch01_array/solution080.go
+++ 10-problems/ch01_array/solution080.go
@@ -1,21 +1,21 @@
 package main
 
-import "fmt"
+import ("fmt")
 
 func removeDuplicates(nums []int) int {
-    k := -1
-    for i := 0; i < len(nums); i++ {
-        if k >= 1 && nums[i] == nums[k] && nums[i] == nums[k-1] {
-            continue
-        }
-        k++
-        nums[k] = nums[i]
-    }
-    return k + 1
+	k := -1
+	for i := 0; i < len(nums); i++ {
+		if k >= 1 && nums[i] == nums[k] && nums[i] == nums[k-1] {
+			continue
+		}
+		k++
+		nums[k] = nums[i]
+	}
+	return k + 1
 }
 
 func main() {
-    nums := []int{1, 1, 1, 2, 2, 3, 3, 3, 3, 3}
-    n := removeDuplicates(nums)
-    fmt.Println(n)
+	nums := []int{1, 1, 1, 2, 2, 3, 3, 3, 3, 3}
+	n := removeDuplicates(nums)
+	fmt.Println(n)
 }
